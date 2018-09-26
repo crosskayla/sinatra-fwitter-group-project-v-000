8f4922ff9d56af043750fddfa463b718cdf3ae7d
Kayla Cross <kac288@georgetown.edu>
2018-09-26 12:51:24 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,14 @@ class UsersController < ApplicationController
     end
   end
 
+  
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+
+
 end
