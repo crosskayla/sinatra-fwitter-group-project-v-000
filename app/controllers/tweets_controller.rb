7404a8651791db9e9239e0026631a082b7950730
Kayla Cross <kac288@georgetown.edu>
2018-09-26 13:08:24 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,14 +9,5 @@ class TweetsController < ApplicationController
     end
   end
 
-  get '/users/:slug' do
-    if logged_in?
-      @user = User.find_by_slug(params[:slug])
-      @tweets = @user.tweets
-      erb :'tweets/tweets'
-    else
-      redirect '/login'
-    end
-  end
 
 end
